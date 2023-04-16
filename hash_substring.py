7bc56c8568ee9974b316ac3ca555c9dc9d7f01e7
Roberts Andris Barlots 221RDB162 <72033044+Roberts-Andris-Barlots-221RDB162@users.noreply.github.com>
2023-04-16 20:58:45 +0000

    test

--- hash_substring.py
+++ hash_substring.py
@@ -3,9 +3,20 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+
     
-    
+    inputted_text = input().strip().lower()
+    if inputted_text == "f":
+        file_path = "./tests/06"
+        with open(file_path, 'r') as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
+
+    elif inputted_text == "i":
+        pattern = input().rstrip()
+        text = input().rstrip()
     # after input type choice
+        
     # read two lines 
     # first line is pattern 
     # second line is text in which to look for pattern 
@@ -13,7 +24,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (pattern, text)
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,9 +32,17 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
-    # and return an iterable variable
-    return [0]
+    occurrences = []
+    pattern_len = len(pattern)
+    text_len = len(text)
+    pattern_hash = hash(pattern)
+    text_hash = hash(text[:pattern_len])
+    for i in range(text_len - pattern_len + 1):
+        if pattern_hash == text_hash and pattern == text[i:i+pattern_len]:
+            occurrences.append(i)
+        if i < text_len - pattern_len:
+            text_hash = hash(text[i+1:i+1+pattern_len])
+    return occurrences
 
 
 # this part launches the functions
