e635ce08035e68506b800cf05955287c9d303f3d
Roberts Andris Barlots 221RDB162 <72033044+Roberts-Andris-Barlots-221RDB162@users.noreply.github.com>
2023-04-16 21:24:34 +0000

    please?

--- hash_substring.py
+++ hash_substring.py
@@ -1,11 +1,8 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-
     
-    inputted_text = input().strip().lower()
+    inputted_text = input()
     if inputted_text == "f":
         file_path = "./tests/06"
         with open(file_path, 'r') as f:
@@ -15,15 +12,6 @@ def read_input():
     elif inputted_text == "i":
         pattern = input().rstrip()
         text = input().rstrip()
-    # after input type choice
-        
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
     return (pattern, text)
 
 def print_occurrences(output):
@@ -36,12 +24,12 @@ def get_occurrences(pattern, text):
     pattern_len = len(pattern)
     text_len = len(text)
     pattern_hash = hash(pattern)
-    text_hash = hash(text[:pattern_len])
+    text_hash = hash(text[: pattern_len])
     for i in range(text_len - pattern_len + 1):
-        if pattern_hash == text_hash and pattern == text[i:i+pattern_len]:
+        if pattern_hash == text_hash and pattern == text[i: i + pattern_len]:
             occurrences.append(i)
         if i < text_len - pattern_len:
-            text_hash = hash(text[i+1:i+1+pattern_len])
+            text_hash = hash(text[i + 1:i + 1 + pattern_len])
     return occurrences
 
 
